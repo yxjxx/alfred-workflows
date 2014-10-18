856705f46cae080389499778bc1c7a11efd00034
yxjxx <yangjing201111335@gmail.com>
2014-10-18 12:40:05 +0800

    fix indentation error caused by newly-installed atom

--- qujing/qujing.sh
+++ qujing/qujing.sh
@@ -3,11 +3,11 @@ proxy_url="http://p.getqujing.com/paths/xxxxxxxx.pac"
 
 case "{query}" in
     1)
-		networksetup -setautoproxyurl "Thunderbolt Ethernet" ${proxy_url}
+    networksetup -setautoproxyurl "Thunderbolt Ethernet" ${proxy_url}
     echo "set qujin for en4"
     ;;
     2)
-		networksetup -setautoproxyurl "Wi-Fi" ${proxy_url}
+    networksetup -setautoproxyurl "Wi-Fi" ${proxy_url}
     echo "set qujin for Wi-Fi"
     ;;
     12)
