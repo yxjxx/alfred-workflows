9d38e1d3e3e856559a84382ca30b0b2eb94a573b
yxjxx <mynameisyxjxx@gmail.com>
2015-03-29 12:01:44 +0800

    add 360Wi-Fi;Finished Fill Pop-up Authorization Dialogs Automatically

--- qujing/README.md
+++ qujing/README.md
@@ -3,6 +3,18 @@ Set qujing autopac url with alfred
 
 - 1 for Thunderbolt Ethernet
 - 2 for Wi-Fi
-- 12 for both
+- 3 for 360Wi-Fi
+- 4 for all
 
-[manual page](https://developer.apple.com/library/mac/documentation/Darwin/Reference/ManPages/man8/networksetup.8.html)
+[Official manual page](https://developer.apple.com/library/mac/documentation/Darwin/Reference/ManPages/man8/networksetup.8.html)
+
+#Commands you may need
+
+```
+networksetup -listallnetworkservices
+```
+![Pic](http://i.imgur.com/OGeBe8V.png)
+
+#Known bug
+
+For Fill Pop-up Authorization Dialogs Automatically, you may need to excute a sudo command first.
--- qujing/qujing.sh
+++ qujing/qujing.sh
@@ -1,18 +1,20 @@
-# set it by yourself
+# set them by yourself
+PASSWD=<Your password>
 proxy_url="http://p.getqujing.com/paths/xxxxxxxx.pac"
 
 case "{query}" in
     1)
-    networksetup -setautoproxyurl "Thunderbolt Ethernet" ${proxy_url}
-    echo "set qujin for en4"
+        echo $PASSWD | sudo -s "networksetup -setautoproxyurl \"Thunderbolt Ethernet\" ${proxy_url}"
     ;;
     2)
-    networksetup -setautoproxyurl "Wi-Fi" ${proxy_url}
-    echo "set qujin for Wi-Fi"
+        echo $PASSWD | sudo -s "networksetup -setautoproxyurl \"Wi-Fi\" ${proxy_url}"
     ;;
-    12)
-    networksetup -setautoproxyurl "Thunderbolt Ethernet" ${proxy_url}
-    networksetup -setautoproxyurl "Wi-Fi" ${proxy_url}
-    echo "set qujin for Wi-Fi and en4"
+    3)
+        echo $PASSWD | sudo -s "networksetup -setautoproxyurl \"802.11 n WLAN\" ${proxy_url}"
+    ;;
+    4)
+        echo $PASSWD | sudo -s "networksetup -setautoproxyurl \"Thunderbolt Ethernet\" ${proxy_url}"
+        echo $PASSWD | sudo -s "networksetup -setautoproxyurl \"Wi-Fi\" ${proxy_url}"
+        echo $PASSWD | sudo -s "networksetup -setautoproxyurl \"802.11 n WLAN\" ${proxy_url}"
     ;;
 esac
