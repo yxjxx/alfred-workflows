5d669a4337987a1d2604882ecaadb32d6628816c
yxjxx <i@yxjxx.com>
2016-04-12 19:34:25 +0800

    6152-->6153

--- surge/set_surge_proxy.sh
+++ surge/set_surge_proxy.sh
@@ -2,11 +2,12 @@
 # @Author: yxjxx
 # @Date:   2016-02-25 21:19:57
 # @Last Modified by:   yxjxx
-# @Last Modified time: 2016-04-12 19:27:36
+# @Last Modified time: 2016-04-12 19:32:12
 # set them by yourself
 PASSWD=<Your_Password>
 domain="127.0.0.1"
 portnumber="6152"
+socksPortNumber="6153"
 domain2="localhost"
 
 echo $PASSWD | sudo -S networksetup -setwebproxy "Thunderbolt Ethernet" ${domain} ${portnumber} off
@@ -18,5 +19,5 @@ echo $PASSWD | sudo -S networksetup -setsecurewebproxy "Wi-Fi" ${domain} ${portn
 echo $PASSWD | sudo -S networksetup -setproxybypassdomains "Thunderbolt Ethernet" ${domain} ${domain2}
 echo $PASSWD | sudo -S networksetup -setproxybypassdomains "Wi-Fi" ${domain} ${domain2}
 
-echo $PASSWD | sudo -S networksetup -setsocksfirewallproxy "Thunderbolt Ethernet" ${domain} ${portnumber}
-echo $PASSWD | sudo -S networksetup -setsocksfirewallproxy "Wi-Fi" ${domain} ${portnumber}
+echo $PASSWD | sudo -S networksetup -setsocksfirewallproxy "Thunderbolt Ethernet" ${domain} ${socksPortNumber}
+echo $PASSWD | sudo -S networksetup -setsocksfirewallproxy "Wi-Fi" ${domain} ${socksPortNumber}
